65097dfcaf799b687a3fb31fba3f6555edb026f8
Phil DeVeau <phil.deveau@incyclesoftware.com>
2019-05-30 16:11:52 -0400

    Iniital Commit With Polly Policy

---
+++ Blazor.Server.Client/Startup.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Http;
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+
+namespace Blazor.Server.Client
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var policy = Policy.Handle<Exception>()
+                .WaitAndRetryAsync(4, currentRetry => TimeSpan.FromMilliseconds(currentRetry * 100));
+
+            services.AddSingleton(provider => new HttpClient(provider.GetService<TokenWebAssemblyMessageHandler>()));
+
+            services.AddHttpClient<ISomeClient, SomeClient>(httpClient =>
+                {
+                    httpClient.BaseAddress = new Uri("https://nope.org");
+                })
+                .AddPolicyHandler(policy.AsAsyncPolicy<HttpResponseMessage>());
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+
+
+    public class SomeClient : ISomeClient
+    {
+        private readonly HttpClient _client;
+
+        public SomeClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<string> GetDataAsync()
+        {
+            var response = await _client.GetAsync("/");
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+
+    public interface ISomeClient
+    {
+        Task<string> GetDataAsync();
+    }
+
+    public class TokenWebAssemblyMessageHandler : WebAssemblyHttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+
+            var uri = request.RequestUri?.ToString() ?? string.Empty;
+
+            if (uri.StartsWith("https://nope.org", StringComparison.InvariantCultureIgnoreCase))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "N/A");
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
